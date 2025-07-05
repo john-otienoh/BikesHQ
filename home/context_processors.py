85a1e2c64d9b69fe8c7ea4f0746ba8d67432b510
john-otienoh <johnteclaire@gmail.com>
2025-07-05 18:26:35 +0300

    minor fixes

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,6 +1,6 @@
 from category.models import Category
 
 def menu_links(request):
-    # links = Category.objects.filter(parent__isnull=True)
-    links=Category.objects.all()
+    links = Category.objects.filter(parent__isnull=False)
+    # links=Category.objects.all()
     return dict(links=links)
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from shop.models import Product
 from category.models import Category
 from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.cache import cache
 
 # Create your views here.
 
@@ -27,15 +28,26 @@ def products(request, category_slug=None):
 
     categories = None
     product_list = None
+    page_obj = 0
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         all_products = Product.objects.filter(category=categories, is_available=True)
         product_count = all_products.count()
+        paginator = Paginator(all_products, 3)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        try:
+            all_products = paginator.page(page_number)
+        except EmptyPage:
+            all_products = paginator.page(paginator.num_pages)
+        except PageNotAnInteger:
+            all_products = paginator.page(1)
     else:
         product_list = Product.objects.filter(is_available=True)
         product_count = product_list.count()
         paginator = Paginator(product_list, 3)
         page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
         try:
             all_products = paginator.page(page_number)
         except EmptyPage:
@@ -46,7 +58,7 @@ def products(request, category_slug=None):
     return render(
         request,
         "home/products.html",
-        {"all_products": all_products, 'product_count': product_count},
+        {"all_products": all_products, 'product_count': product_count, 'page_obj': page_obj},
     )
 
 
